16a980a88a38d34971f86d814eea96f6e5a43ed0
TimothyLewis <timlew2003@gmail.com>
2024-09-06 14:58:19 -0700

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+const pineTree = document.createElement('img');
+pineTree.src = 'assets/pine-tree.png';
+pineTree.style.position = 'absolute';
+pineTree.style.left = '450px';
+pineTree.style.bottom = '200px';
+document.body.append(pineTree);
+
+function newImage(src, left, bottom) {
+    const img = document.createElement('img');
+    img.src = src;
+    img.style.position = 'absolute';
+    img.style.left = left;
+    img.style.bottom = bottom;
+    document.body.append(img);
+    return img;
+}
+
+// Use newImage to add images
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
